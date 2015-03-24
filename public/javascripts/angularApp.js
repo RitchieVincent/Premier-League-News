8d1648320d450d892c7eedf36c5071fe578c2046
emRitchie Vincent/em <emrfcvincent@gmail.com/em>
2015-03-24 12:52:08 +0000

    # in url, not displaying views

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,106 @@
+var app = angular.module('flapperNews', ['feeds', 'ui.router']);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl',
+                resolve: {
+                    postPromise: ['posts', function (posts) {
+                        return posts.getAll();
+                }]
+                }
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+
+
+        $urlRouterProvider.otherwise('home');
+    }]);
+
+app.factory('posts', ['$http', function ($http) {
+    var o = {
+        posts: []
+    };
+    return o;
+    o.getAll = function () {
+        return $http.get('/posts').success(function (data) {
+            angular.copy(data, o.posts);
+        });
+    };
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data){
+            o.posts.push(data);
+        });
+    };
+}]);
+
+app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function ($scope, posts) {
+        $scope.posts = posts.posts;
+        $scope.posts = [
+            {
+                title: 'post 1',
+                upvotes: 5
+            },
+            {
+                title: 'post 2',
+                upvotes: 2
+            },
+            {
+                title: 'post 3',
+                upvotes: 15
+            },
+            {
+                title: 'post 4',
+                upvotes: 9
+            },
+            {
+                title: 'post 5',
+                upvotes: 4
+            }
+        ];
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === '') { return; }
+            posts.create({
+                title: $scope.title,
+                link: $scope.link,
+            });
+            $scope.title = '';
+            $scope.link = '';
+        };
+        $scope.incrementUpvotes = function (post) {
+            post.upvotes += 1;
+        };
+    }]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function ($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+
+        $scope.addComment = function () {
+            if ($scope.body === '') {
+                return;
+            }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
+
+    }]);
