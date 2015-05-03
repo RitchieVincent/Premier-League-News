865c13cad6b7389735f3808e3eb9cefb773b1a92
emRitchie Vincent/em <emrfcvincent@gmail.com/em>
2015-05-03 19:10:12 +0100

    Transparent statusbar added

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,19 @@
-angular.module('footFeeds', ['ionic', 'footFeeds.controllers', 'footFeeds.services', 'ionic.contrib.drawer'])
+angular.module('footFeeds', ['ionic', 'footFeeds.controllers', 'footFeeds.services', 'ionic.contrib.drawer', 'ngCordova'])
 
-.run(function ($ionicPlatform) {
+.run(function ($ionicPlatform, $cordovaStatusbar) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
+
+        if (StatusBar && statusbarTransparent) {
+            // Enable translucent statusbar
+            statusbarTransparent.enable();
+
+            // Get the bar back
+            StatusBar.show();
         }
     });
 })
